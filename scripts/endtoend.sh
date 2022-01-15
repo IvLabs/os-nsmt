597c8d3d8f2fa8412984c246eba482c455ccf7b9
Rohit Lal <take2rohit@gmail.com>
2022-01-15 19:16:20 +0530

    completed centroids + source only training

--- scripts/endtoend.sh
+++ scripts/endtoend.sh
@@ -1,6 +1,7 @@
 # for prototypes
 CUDA_VISIBLE_DEVICE=0 python src/embeddings.py --wandb --output_dir ./run2
-# for warmup (will save 3 weights netF, netB, netC)
-python image_source_final.py --gpu_id 2  --dataset OfficeHome --output weights --batch_size 128 --max_epoch 50 --source Ar,Pr --target Cl,Rw --wandb 0
-
+# for source only training (will save 3 weights netF, netB, netC)
+CUDA_VISIBLE_DEVICE=0 python image_source_final.py --dataset OfficeHome --output weights --batch_size 128 --max_epoch 50 --source Ar,Pr --target Cl,Rw --wandb 0
+# for computing centroids and saving TSNE plots
+CUDA_VISIBLE_DEVICE=0 python warmup.py --dataset OfficeHome --source Ar,Pr --target Cl,Rw --trained_wt weights/uda/OfficeHome
 # for adaptation
